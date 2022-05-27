2649142dc654d9378e827e95c41303831fc35cc0
anugrahkresnaya <anugrahkresnaya@gmail.com>
2022-05-27 10:03:17 +0700

    first commit

---
+++ components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import styles from './Navbar.module.css';
+import Image from 'next/image';
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <nav className={styles.navbar}>
+        <Image 
+          src='/images/logo.png'
+          height={60}
+          width={350}
+        />
+        <ul className={styles.ul}>
+          <li className={`${styles.li} ${styles.covid}`}>Info Covid-19</li>
+          <li className={styles.li}>Home</li>
+          <li className={styles.li}>Akreditasi Prodi</li>
+          <li className={styles.li}>Prosedur umum</li>
+          <li className={styles.li}>FAQ</li>
+          <li className={styles.li}>Pendaftaran</li>
+          <li className={styles.li}>Login</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
